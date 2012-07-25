fb5f27a43502f30da78b1ea9f39272bb8c41997c
matias <matidae@gmail.com>
2012-07-25 11:24:24 -0300

    up

--- rnaseqsim.py
+++ rnaseqsim.py
@@ -9,21 +9,26 @@ readsvar = sys.argv[1]
 gene_expression = sys.argv[2]
 utr5 = 50
 utr3 = 50
+read_length = int(sys.argv[3])
 
+#Parsing GFF file
 gff_parser.parse(gffvar, gene_expression, utr5, utr3)
 
 expresion_list = map(lambda x: int(x.rsplit()[1]), open(gene_expression, "r"))
-total_reads = sum(expresion_list)
+#total_reads = sum(expresion_list)
 
 def __printReads(gffvar, coef_var):
     n=0
-    exp_file = open(readsvar+".exp_normal","w")
+#    exp_file = open(readsvar+".exp_normal","w")
     reads_file = open(readsvar+".fasta","w")
-    for i in SeqIO.parse(gffvar + ".utr.fasta", "fasta"):
+#    for i in SeqIO.parse(gffvar + ".utr.fasta", "fasta"):
+    b = open("brucei.gff.utr.fasta","r")
+    for i in SeqIO.parse(b, "fasta"):
         read_var = round(distrib.valueNormal(coef_var, int(expresion_list[n])))
-        reads_list = splicer.generate_reads(i.seq, int(read_var))
-        exp_normal = (Decimal(read_var) / len(i.seq)) / total_reads
-        exp_file.write(i.name + "\t" + str(round(exp_normal*10000, 4)) + "\n")
+        reads_list = splicer.generate_reads(i.seq, int(read_var), read_length)
+        # exp_normal en RPKM
+#        exp_normal = (Decimal(str(read_var)) / len(i.seq)) / total_reads
+#        exp_file.write(i.name + "\t" + str(round(exp_normal*10000, 4)) + "\n")
         j = 0
         for read in reads_list:
             reads_file.write(">" + i.name + "_read_"+ str(j) + "\n")
@@ -31,7 +36,7 @@ def __printReads(gffvar, coef_var):
             j+=1
         n+=1
 
-__printReads(gffvar, 0.2)
+__printReads(gffvar, 0.0025)
 def test(coef_var, exp, out):
     exp_file = open(out,"w")
     expresion_list = map(lambda x: int(x.rsplit()[1]), open(exp, "r"))
--- splicer.py
+++ splicer.py
@@ -10,40 +10,70 @@ def __weighted_index(weights):
 
 #Wang et al. (2009) RNA-Seq: a revolutionary tool for transcriptomics. Nature reviews. Genetics, 10(1), pp.57-63.
 #RNA fragmentation
-def __coverage_bias (gene_len):
+def __coverage_bias (gene_len, read_length):
     weigth = []
-    for i in xrange(0, gene_len-30):
-        if i >= 0 and i < gene_len/10:
-            weigth.append(0.1/(gene_len/10))
-        elif i >= gene_len/10 and i < (gene_len - (gene_len/10)):
-            weigth.append(0.5/(gene_len - (gene_len/10)))
+    for i in xrange(0, gene_len-read_length):
+        if i >= 0 and i < gene_len/5:
+            weigth.append(0.05/(gene_len/5))
+        elif i >= gene_len/5 and i < (gene_len - (gene_len/5)):
+            weigth.append(0.9/(gene_len - (gene_len/5)))
         else:
-            weigth.append(0.1/(gene_len/10))
+            weigth.append(0.05/(gene_len/5))
     return weigth
- 
-#Dohm et al.(2008) Substantial biases in ultra-short read data sets from high-throughput DNA sequencing. Nucleic Acids Research, 36(16), p.e105.
-def __gc_bias(sequence, weigth):
+
+#Aird, D. et al. (2011) Analyzing and minimizing PCR amplification bias in Illumina sequencing libraries. Genome Biology, 12(2) 
+def __gc_bias(sequence, weigth, read_length):
     aux_read = ""
     aux_gc = 0
-    for i in xrange(0,3):
+    for i in xrange(0,10):
         pos = __weighted_index(weigth)
-        read = sequence[pos:pos+30]
+        read = sequence[pos:pos+read_length]
         gc = (read.count("G") + read.count("C")) / float(len(read))
-        if aux_gc <= gc:
+        if gc >= 0.12 and gc <= 0.65:
+            return read
+        else:
             aux_read = read
-            aux_gc = gc
-    return aux_read
+        return aux_read
+
+#Dohm et al.(2008) Substantial biases in ultra-short read data sets from high-throughput DNA sequencing. Nucleic Acids Research, 36(16), p.e105.
+def __weigth_errors(read_length):
+    weigth_pos = list()
+    n = 0
+    while n <= read_length * .5:
+        weigth_pos.append(0.01/(read_length*.5))
+        n+=1
+    while n > read_length * .5 and n <= read_length * .6:
+        weigth_pos.append(0.015/(read_length*.6 - read_length*.5))
+        n+=1
+    while n > read_length * .6  and n <= read_length * .7:
+        weigth_pos.append(0.02/(read_length*.7 - read_length*.6))
+        n+=1
+    while n > read_length * .7 and n <= read_length * .8:
+        weigth_pos.append(0.03/(read_length*.8 - read_length*.7))
+        n+=1
+    while n > read_length * .8 and n <= read_length * .9:
+        weigth_pos.append(0.04/(read_length*.9 - read_length*.8))
+        n+=1
+    while n > read_length * .9 and n <= read_length * .95:
+        weigth_pos.append(0.065/(read_length*.95 - read_length*.9))
+        n+=1
+    while n > read_length * .95 and n < read_length:
+        weigth_pos.append(0.085/(read_length - read_length*.95))
+        n+=1
+    weigth_pos.append(50)
+    return weigth_pos
 
 def __seq_error(read):
-    weigth_pos = [0.028, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.012, 0.015, 0.017, 0.02, 0.019, 0.021, 0.022, 0.025, 0.03, 0.035, 0.04, 0.042, 0.061, 0.065, 0.082, 0.087, 70]    
+    weigth_pos = __weigth_errors(len(read))
     pos = __weighted_index(weigth_pos)
-    if pos != 30:
+    if pos != len(read):
         return __wrong_base_call(read, pos)
     else:
         return read    
 
 def __wrong_base_call(read, pos):
     read_aux = list(read)
+    #print str(len(read)) + "-----"+ str(pos)
     if read_aux[pos] == "A":
         err = __weighted_index([0.05, 0.1, 0.25])#A>G, A>T, A>C
         if err == 0:
@@ -83,12 +113,12 @@ def __wrong_base_call(read, pos):
     read_err = "".join(read_aux)
     return read_err
 
-def generate_reads(sequence, numreads):
+def generate_reads(sequence, numreads, read_length):
     gene_len = len(sequence)
-    weigth = __coverage_bias(gene_len)
+    weigth = __coverage_bias(gene_len, read_length)
     read_list = []
     for i in xrange(0, numreads):
-        read = __gc_bias(sequence, weigth)
+        read = __gc_bias(sequence, weigth, read_length)
         read_seq =  __seq_error(read)
         read_list.append(read_seq)
     return read_list
